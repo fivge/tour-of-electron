9a625067ba7381f19ea1ce54af4695be580a1ccd
fivge <luanxingtong@gmail.com>
2022-09-18 02:30:03 +0800

    feat: quick start

---
+++ README.md
@@ -0,0 +1,21 @@
+https://www.electronjs.org/zh/docs/latest/tutorial/quick-start
+
+### TODO
+
+#### framework
+
+https://github.com/maximegris/angular-electron
+
+#### UI
+
+https://arwes.dev/design/
+
+https://github.com/arwes/arwes
+
+https://augmented-ui.com/
+
+https://github.com/propjockey/augmented-ui
+
+#### windows API
+
+https://learn.microsoft.com/zh-cn/windows/apps/design/shell/tiles-and-notifications/primary-tile-apis
--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 const { app, BrowserWindow } = require('electron');
 
+const path = require('path');
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+      // __dirname 字符串指向当前正在执行脚本的路径 (在本例中，它指向你的项目的根文件夹)。
+      // path.join API 将多个路径联结在一起，创建一个跨平台的路径字符串。
+    }
   });
 
   win.loadFile('index.html');
 };
 
+// 窗口无法在 ready 事件前创建
 app.whenReady().then(() => {
   createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// 在Windows和Linux上，关闭所有窗口通常会完全退出一个应用程序。
+// 为了实现这一点，你需要监听 app 模块的 'window-all-closed' 事件。如果用户不是在 macOS(darwin) 上运行程序，则调用 app.quit()。
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
